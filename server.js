129712a55b613a3e4a986a06d06f2bace7723477
Andrew Jiang <andrewji@usc.edu>
2016-02-27 12:10:13 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var MongoStore = require('connect-mongo')(session);
+
+require('./config/mongoose.js');
+require('./config/routes.js')(app);
+
+var mongoose = require('mongoose');
+
+http.listen(8000, function(){
+	console.log("Server up and running on port 8000");
+})
