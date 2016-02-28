f6c7520d102ceecf5ceb66740c4b7f2658d17081
Andrew Jiang <andrewji@usc.edu>
2016-02-27 16:21:46 -0800

    thingy

--- config/routes.js
+++ config/routes.js
@@ -28,7 +28,7 @@ module.exports = function(app){
 			sendmms: true,
 			showauthurl: false,
 			authstate: null,
-			text: JSON.stringify(req),
+			text: req,
 			speech: "Hello World! You said \"Hello World!\".",
 			status: "OK",
 			webhookreply: null,
